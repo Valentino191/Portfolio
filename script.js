e5df7b1c5698ac8bf6a23134dcd5be83540db338
Valentino191 <129868159+Valentino191@users.noreply.github.com>
2025-04-16 17:38:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const themeSwitch = document.querySelector('.theme-switch');
+const htmlElement = document.documentElement;
+const moonIcon = '<i class="fas fa-moon"></i>';
+const sunIcon = '<i class="fas fa-sun"></i>';
+
+
+const savedTheme = localStorage.getItem('theme') || 'dark';
+htmlElement.setAttribute('data-theme', savedTheme);
+themeSwitch.innerHTML = savedTheme === 'dark' ? moonIcon : sunIcon;
+
+
+themeSwitch.addEventListener('click', () => {
+    const currentTheme = htmlElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    
+    htmlElement.setAttribute('data-theme', newTheme);
+    themeSwitch.innerHTML = newTheme === 'dark' ? moonIcon : sunIcon;
+    
+    
+    localStorage.setItem('theme', newTheme);
+});
